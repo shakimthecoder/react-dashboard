a41aa626e48113c2ad0e4fa6eeaf7d0dd6b8e953
Shakim Harris <amenraprod@gmail.com>
2025-03-23 20:19:05 -0400

    rt fetchwrapper

--- src/providers/data/index.tsx
+++ src/providers/data/index.tsx
@@ -5,6 +5,7 @@ const API_URL = 'http://api.crm.refine.dev'
 export const client = new GraphQLClient(API_URL, {
     fetch(url: string, options: RequestInit) => {
         try {
+          return fetchWrapper(url, options);
         } catch (error) {
            return Promise.reject(error as Error)
         }

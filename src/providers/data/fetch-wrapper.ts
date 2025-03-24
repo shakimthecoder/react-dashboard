a3bbb957ac14308c4404db973b1b752bf7174516
Shakim Harris <amenraprod@gmail.com>
2025-03-23 20:47:14 -0400

    finish fetch wrapper

--- src/providers/data/fetch-wrapper.ts
+++ src/providers/data/fetch-wrapper.ts
@@ -39,3 +39,15 @@ const getGraphQLErrors = (body: Record<"errors", GraphQLFormattedError | undefin
     }
     return null;
 }
+
+export const fetchWrapper = async (url: string, options: RequestInit) => {
+    const response = await customFetch(url, options);
+    const responseClone = response.clone();
+    const body = await responseClone.json();
+    const error = getGraphQLErrors(body);
+
+    if(error){
+        throw error;
+       }
+    else return response;
+}

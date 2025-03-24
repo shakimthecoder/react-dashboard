599910e9eaddeeecfdb0c8905499def746fca121
Shakim Harris <amenraprod@gmail.com>
2025-03-24 13:52:25 -0400

    fixed websockets

--- src/providers/data/index.tsx
+++ src/providers/data/index.tsx
@@ -1,33 +1,44 @@
-import GraphQLDataProvider, { GraphQLClient } from "@refinedev/nestjs-query";
-import { createClient } from 'graphql-ws'
-
-export const API_BASE_URL = 'http://api.crm.refine.dev';
-export const API_URL = 'http://api.crm.refine.dev';
-export const WSS_URL = 'wss://api.crm.refine.dev/graphql';
-
-export const client = new GraphQLClient(API_URL, {
-    fetch(url: string, options: RequestInit) => {
-        try {
-          return fetchWrapper(url, options);
-        } catch (error) {
-           return Promise.reject(error as Error)
-        }
-    }
-    })
-
-export const wsClient = typeof window !== undefined 
-? createClient({
-    url: WSS_URL,
-    connectionParams: () => {
-        const accessToken - localStorage.getItem('access_token');
-    }
-    return {
-        headers: {
-            Authorization: `Bearer ${accessToken}`;
-        }
-    }
-}): undefined 
-
-export const DataProvider = GraphQLDataProvider(client);
-
+import graphqlDataProvider, {
+    GraphQLClient,
+    liveProvider as graphqlLiveProvider,
+  } from "@refinedev/nestjs-query";
   
+  import { createClient } from "graphql-ws";
+  
+  import { fetchWrapper } from "./fetch-wrapper";
+  
+  export const API_BASE_URL = "https://api.crm.refine.dev";
+  export const API_URL = `${API_BASE_URL}/graphql`;
+  export const WS_URL = "wss://api.crm.refine.dev/graphql";
+  
+  export const client = new GraphQLClient(API_URL, {
+    fetch: (url: string, options: RequestInit) => {
+      try {
+        return fetchWrapper(url, options);
+      } catch (error) {
+        return Promise.reject(error as Error);
+      }
+    },
+  });
+  
+  export const wsClient =
+    typeof window !== "undefined"
+      ? createClient({
+          url: WS_URL,
+          connectionParams: () => {
+            const accessToken = localStorage.getItem("access_token");
+  
+            return {
+              headers: {
+                Authorization: `Bearer ${accessToken}`,
+              },
+            };
+          },
+        })
+      : undefined;
+  
+  export const dataProvider = graphqlDataProvider(client);
+  
+  export const liveProvider = wsClient
+    ? graphqlLiveProvider(wsClient)
+    : undefined;

7403aa32c5c08e60f5ece6019095f71f75857a67
Shakim Harris <amenraprod@gmail.com>
2025-03-23 20:20:06 -0400

    create type for header

--- src/providers/data/fetch-wrapper.ts
+++ src/providers/data/fetch-wrapper.ts
@@ -1,3 +1,4 @@
 export const customFetch = async (url: string, options: RequestInit) => {
     const accessToken: LocalStorage.getItem('access_token');
+    const headers = options.headers as Record<string, string>;
 }

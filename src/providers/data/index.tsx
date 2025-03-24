4990080d634489ebeaefd8d8c8b333f773119bde
Shakim Harris <amenraprod@gmail.com>
2025-03-23 21:06:45 -0400

    added data provider

--- src/providers/data/index.tsx
+++ src/providers/data/index.tsx
@@ -1,7 +1,10 @@
-import { GraphQLClient } from "@refinedev/nestjs-query";
+import GraphQLDataProvider, { GraphQLClient } from "@refinedev/nestjs-query";
+import { createClient } from 'graphql-ws'
 
+export const API_BASE_URL = 'http://api.crm.refine.dev';
+export const API_URL = 'http://api.crm.refine.dev';
+export const WSS_URL = 'wss://api.crm.refine.dev/graphql';
 
-const API_URL = 'http://api.crm.refine.dev'
 export const client = new GraphQLClient(API_URL, {
     fetch(url: string, options: RequestInit) => {
         try {
@@ -9,8 +12,22 @@ export const client = new GraphQLClient(API_URL, {
         } catch (error) {
            return Promise.reject(error as Error)
         }
+    }
+    })
+
+export const wsClient = typeof window !== undefined 
+? createClient({
+    url: WSS_URL,
+    connectionParams: () => {
+        const accessToken - localStorage.getItem('access_token');
+    }
+    return {
+        headers: {
+            Authorization: `Bearer ${accessToken}`;
         }
     }
-});
+}): undefined 
 
+export const DataProvider = GraphQLDataProvider(client);
 
+  

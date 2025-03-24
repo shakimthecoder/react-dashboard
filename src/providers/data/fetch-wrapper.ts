09ebfc783735cd146f3e9cfcdb8f220cb18bd850
Shakim Harris <amenraprod@gmail.com>
2025-03-23 20:40:10 -0400

    created type

--- src/providers/data/fetch-wrapper.ts
+++ src/providers/data/fetch-wrapper.ts
@@ -1,5 +1,10 @@
 import{ GraphQLFormattedError } from "graphql";
 
+type Error = {
+    message: string,
+    statusCode: string
+};
+
 export const customFetch = async (url: string, options: RequestInit) => {
     const accessToken = window.localStorage.getItem('access_token');
     const headers = options.headers as Record<string, string>;
@@ -14,4 +19,22 @@ export const customFetch = async (url: string, options: RequestInit) => {
     })
 }
 
-const getGraphQLErrors = (body: Record<"errors", GraphQLFormattedError) => []
+const getGraphQLErrors = (body: Record<"errors", GraphQLFormattedError | undefined>): Error | null => {
+    if(!body){
+        return {
+            message: "Unknown error",
+            statusCode: "INTERNAL_SERVER_ERROR"
+        }
+    }
+    if("errors" in body){
+        const errors = body?.errors;
+        const messages = errors?.map((error) => error?.message)?.join('');
+        const code = errors?.[0]?.extensions?.code;
+
+        return {
+            message: messages || JSON.stringify(errors);
+            statusCode: code || 500
+        }
+
+    }
+}

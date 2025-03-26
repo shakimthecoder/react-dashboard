64426dd2542a358e146e11fbf0c1ebd4037aad34
Shakim Harris <amenraprod@gmail.com>
2025-03-25 20:51:00 -0400

    add content with text styling

--- src/components/layout/current-user.tsx
+++ src/components/layout/current-user.tsx
@@ -1,13 +1,32 @@
 import React from 'react'
 import { Popover } from 'antd'
 import CustomAvatar from '../custom-avatar'
-import { useGetIdentity } from '@refinedev/core';}
+import { useGetIdentity } from '@refinedev/core';
 
 import type { User } from "@/graphql/schema.types";
+import { Text } from '../text'
 
 
 export const CurrentUser = () => {
     const { data: user } = useGetIdentity<User>();
+
+    const content = () => {
+        return (
+            <div 
+            style={{ 
+            display: 'flex',
+            flexDirection: 'column'
+            }}
+            >
+            <Text
+            strong 
+            style={{ padding: '10px 20px'}}
+            >
+            {user?.name}
+            </Text>
+            </div>
+        )
+    }
     return ( 
         <>
         <Popover

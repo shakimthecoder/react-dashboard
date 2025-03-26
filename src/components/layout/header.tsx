8e6820974fcc401c2cc22d21501adef853535aac
Shakim Harris <amenraprod@gmail.com>
2025-03-25 23:04:05 -0400

    added css

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -2,6 +2,21 @@ import React from 'react'
 import { Layout, Space } from "antd"
 import { CurrentUser } from './current-user';
 
+
+const headerStyles: React.CSSProperties = {
+   background: '#fff',
+   display: 'flex',
+   justifyContent: 'flex-end',
+   alignItems: 'center',
+   padding: '0 24px',
+   position: "sticky",
+   top: 0,
+   zIndex: 999
+
+
+
+}
+
 function Header() {
   return (
     <div>

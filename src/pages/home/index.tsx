5a5b6c11725702b583fb2560da4fd063acaa7605
Shakim Harris <amenraprod@gmail.com>
2025-03-26 18:33:09 -0400

    style rows

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -3,11 +3,16 @@ import { Col, Row } from "antd"
 export const Home = () => {
     return (
     <div>
-        <Row>
+        <Row
+        gutter={[32, 32]}
+        style={{ marginTop: '32px' }}>
         <Col
         xs={24}
         sm={24}
         xl={8}
+        style=
+        {{ height: '460px' 
+        }}
         >
             CalendarUpcomingEvents
         </Col>
@@ -15,6 +20,9 @@ export const Home = () => {
         xs={24}
         sm={24}
         xl={8}
+        style=
+        {{ height: '460px' 
+        }}
         >
             DashboardDealsChart
         </Col>

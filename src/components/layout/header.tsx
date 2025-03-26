e28453543a582637f1ca0ec49ecee1f965c217e7
Shakim Harris <amenraprod@gmail.com>
2025-03-25 22:59:42 -0400

    wrap header

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import { Layout, Space } from "antd"
 import { CurrentUser } from './current-user';
 
 function Header() {
   return (
+    <div>
+    <Layout.Header>
+      <Space / >
     <CurrentUser />
+    </Layout.Header>
+    </div>
   )
 }
 
